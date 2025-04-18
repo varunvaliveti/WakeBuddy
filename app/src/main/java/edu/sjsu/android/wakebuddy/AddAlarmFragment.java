967e1264724172fa232cbaa7ea287eed4b123131
Tien Pham <97005404+Tienbruh@users.noreply.github.com>
2025-04-18 10:57:37 -0700

    Fixed Add Alarm Radio Buttons

--- app/src/main/java/edu/sjsu/android/wakebuddy/AddAlarmFragment.java
+++ app/src/main/java/edu/sjsu/android/wakebuddy/AddAlarmFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 public class AddAlarmFragment extends Fragment {
 
@@ -28,6 +29,12 @@ public class AddAlarmFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_alarm, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_alarm, container, false);
+        RadioGroup radioGroup = view.findViewById(R.id.radioGroup);
+
+        // Set the default checked RadioButton
+        radioGroup.check(R.id.physicalAddAlarm);
+
+        return view;
     }
 }
